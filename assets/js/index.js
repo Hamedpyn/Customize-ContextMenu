a34387a22bbc8a6cd65a1ced21bfa50c02cf13cd
Hamed <hamedpyn@gmail.com>
2023-08-05 14:06:22 +0430

    show a customize context menu by right click

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// Select DOM elements
+
+let contextMenu = document.querySelector('#contextMenu')
+
+document.addEventListener('contextmenu', item => {
+    item.preventDefault()
+    contextMenu.style.display = 'block'
+    contextMenu.style.left = item.pageX + 'px'
+    contextMenu.style.top = `${item.pageY}px`
+})
