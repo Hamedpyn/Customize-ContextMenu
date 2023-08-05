10825ba4cf4505a84f00f5f25478a11272e91083
Hamed <hamedpyn@gmail.com>
2023-08-05 14:10:15 +0430

    commenting

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,18 @@
 
 let contextMenu = document.querySelector('#contextMenu')
 
-document.addEventListener('contextmenu', item => {
+// Handle 'contextmenu' right click event
+document.addEventListener('contextmenu', (item) => {
+    // Prevent default browser context menu
     item.preventDefault()
+    // Show custom context menu
     contextMenu.style.display = 'block'
+    // Position context menu under click location
     contextMenu.style.left = item.pageX + 'px'
-    contextMenu.style.top = `${item.pageY}px`
+    contextMenu.style.top = item.pageY + 'px'
 })
 
+// Hide context menu by any click on body
 document.body.onclick = function () {
     contextMenu.style.display = 'none'
 }
